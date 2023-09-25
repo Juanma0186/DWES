3f7e96dc2aa00d2ea3a225cb7496e3d4990dc76a
Juanma0186 <juanma0186@gmail.com>
2023-09-25 20:44:55 +0200

    Hecho el ejercicio 2 de funciones

---
+++ TEMA_2/PRACTICAS/02-integracion/funciones/ejercicio1.php
@@ -0,0 +1,32 @@
+<?php
+
+function concatenar(string ...$cadenas): string
+{
+
+  $cadena_final = "";
+
+  foreach ($cadenas as $cadena) {
+    $cadena_final += " " + $cadena;
+  }
+
+  return $cadena_final;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Funciones 1</title>
+</head>
+
+<body>
+  <p>
+    <?= concatenar("Hola", "que", "tal", "estas", "?") ?>
+  </p>
+</body>
+
+</html>
