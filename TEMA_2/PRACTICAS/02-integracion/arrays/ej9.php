6e693f86afc23bcf2474e2ab8faa4471b6a420a8
Juanma0186 <juanma0186@gmail.com>
2023-10-03 20:39:03 +0200

    Añadido la ruta del archivo a una constante

--- TEMA_2/PRACTICAS/02-integracion/arrays/ej9.php
+++ TEMA_2/PRACTICAS/02-integracion/arrays/ej9.php
@@ -1,7 +1,8 @@
 <?php
+define("FILE_NAME", "store.dat");
 //Comprobamos que el archivo existe y que no esta vacio
-if (file_exists('store.dat') && filesize('store.dat') > 0) {
-  $s = file_get_contents('store.dat');
+if (file_exists('FILE_NAME') && filesize('FILE_NAME') > 0) {
+  $s = file_get_contents('FILE_NAME');
   $tareas = unserialize($s);
 } else {
   //Si no existe o esta vacio creamos un array vacio de tareas
@@ -15,7 +16,7 @@ function añadirTarea($nombre, $fecha, $asignatura, $descripcion)
 
   //Guardamos el array en el archivo
   $s = serialize($tareas);
-  file_put_contents('store.dat', $s);
+  file_put_contents('FILE_NAME', $s);
 }
 
 if (isset($_POST['añadir'])) {
