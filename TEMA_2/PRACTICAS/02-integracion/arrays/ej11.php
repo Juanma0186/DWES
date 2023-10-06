9b6bae2ca0bbcf7a337cc76f7e475b5730d49cfb
Juanma0186 <juanma0186@gmail.com>
2023-10-06 16:18:08 +0200

    Cambios en el ejercicio 11

---
+++ TEMA_2/PRACTICAS/02-integracion/arrays/ej11.php
@@ -0,0 +1,56 @@
+<?php
+
+function aplastalo(...$valores)
+{
+  $result = [];
+  foreach ($valores as $valor) {
+    if (is_array($valor)) {
+      $result = array_merge($result, aplastalo(...$valor));
+    } else {
+      $result[] = $valor;
+    }
+  }
+  return $result;
+}
+
+function aplastalo2(...$valores)
+{
+  $result = [];
+  $valores = [[0, 3], 42];
+
+  while (!empty($valores)) {
+    $element = array_shift($valores);
+
+    if (is_array($element)) {
+      foreach (array_reverse($element) as $subelement) {
+        array_unshift($valores, $subelement);
+      }
+    } else {
+      $result[] = $element;
+    }
+  }
+
+  return $result;
+}
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <p>
+    El array tiene [<?= implode(",", aplastalo2([0], 42)) ?>]
+  </p>
+</body>
+
+</html>
