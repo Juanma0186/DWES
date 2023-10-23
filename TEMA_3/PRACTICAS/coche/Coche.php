95678a3b52dcf9bfd8552a4aac95671253e3b70a
Juanma0186 <juanma0186@gmail.com>
2023-10-23 21:03:32 +0200

    Cambios menores

--- TEMA_3/PRACTICAS/coche/Coche.php
+++ TEMA_3/PRACTICAS/coche/Coche.php
@@ -3,56 +3,56 @@
 class Coche
 {
 
-  //Atributos
-  private $matricula;
-  private $marca;
-  private $carga;
+    //Atributos
+    private $matricula;
+    private $marca;
+    private $carga;
 
-  //Constructor
+    //Constructor
 
-  public function __construct(string $matricula, string $marca, int $carga)
-  {
-    $this->matricula = $matricula;
-    $this->marca = $marca;
-    $this->carga = $carga;
-  }
+    public function __construct(string $matricula, string $marca, int $carga)
+    {
+        $this->matricula = $matricula;
+        $this->marca = $marca;
+        $this->carga = $carga;
+    }
 
-  //Getters y Setters
+    //Getters y Setters
 
-  public function getMasca(): string
-  {
-    return $this->marca;
-  }
+    public function getMasca(): string
+    {
+        return $this->marca;
+    }
 
-  public function setMarca(string $marca): void
-  {
-    $this->marca = $marca;
-  }
+    public function setMarca(string $marca): void
+    {
+        $this->marca = $marca;
+    }
 
-  public function getMatricula(): string
-  {
-    return $this->matricula;
-  }
+    public function getMatricula(): string
+    {
+        return $this->matricula;
+    }
 
-  public function setMatricula(string $matricula): void
-  {
-    $this->matricula = $matricula;
-  }
+    public function setMatricula(string $matricula): void
+    {
+        $this->matricula = $matricula;
+    }
 
-  public function getCarga(): int
-  {
-    return $this->carga;
-  }
+    public function getCarga(): int
+    {
+        return $this->carga;
+    }
 
-  public function setCarga(int $carga): void
-  {
-    $this->carga = $carga;
-  }
-  //Métodos
+    public function setCarga(int $carga): void
+    {
+        $this->carga = $carga;
+    }
+    //Métodos
 
-  public function __toString(): string
-  {
-    $cadena = "Coche: $this->matricula, $this->marca con carga: $this->carga";
-    return $cadena;
-  }
+    public function __toString(): string
+    {
+        $cadena = "Coche: $this->matricula, $this->marca con carga: $this->carga";
+        return $cadena;
+    }
 }
