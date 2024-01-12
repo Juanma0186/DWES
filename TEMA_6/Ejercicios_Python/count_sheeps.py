13ef11ff1032da24622ca0214ea1f2a77bcbc161
Juanma0186 <juanma0186@gmail.com>
2024-01-12 21:32:50 +0100

    Add 2 ejercicios

---
+++ TEMA_6/Ejercicios_Python/count_sheeps.py
@@ -0,0 +1,16 @@
+# ***************
+# CONTANDO OVEJAS
+# ***************
+
+
+def run(num_sheeps: int) -> str:
+    # TU CÓDIGO AQUÍ
+    sleep = ""
+    for i in range(num_sheeps):
+        sleep += "sheep..." 
+
+    return sleep
+
+
+if __name__ == '__main__':
+    run(0)
