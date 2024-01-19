f6228bd1f885bd5a005dcdb5447476595527775f
Juanma0186 <juanma0186@gmail.com>
2024-01-19 17:59:48 +0100

    DELETE pycache y actulizado gitign

--- TEMA_6/Ejercicios_Python/Funciones/consecutive_freqs.py
+++ TEMA_6/Ejercicios_Python/Funciones/consecutive_freqs.py
@@ -4,25 +4,32 @@
 
 
 def cfreq(items: list ,/, as_string=False):
-    # TU CÓDIGO AQUÍ
-    # Casos vacíos
+# Casos vacíos
     if not items:
         return [] if not as_string else ''
 
+# Resto de casos
     result = []
+
+    # Inicializamos el primer elemento
     item_Actual = items[0]
     cont = 1
 
+    # Recorremos la lista desde el segundo elemento
     for item in items[1:]:
+        # Si es igual, incrementamos el contador
         if item == item_Actual:
             cont += 1
+        # Si es distinto, guardamos el nuevo elemento y reiniciamos contador
         else:
             result.append((item_Actual, cont))
             item_Actual = item
             cont = 1
 
+    # Guardamos el último elemento y su contador
     result.append((item_Actual, cont))
 
+    # Devolvemos el resultado según as_string
     if as_string:
         return ','.join(f'{item}:{cont}' for item, cont in result)
     else:
